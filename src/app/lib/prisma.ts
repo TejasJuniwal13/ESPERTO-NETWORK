dfa92601fcb77550bc4da9b89923fc37670d7e5f
TejasJuniwal13 <tejasjuniwal13@gmail.com>
2025-12-29 16:06:51 +0530

    r

--- src/app/lib/prisma.ts
+++ src/app/lib/prisma.ts
@@ -1,46 +1,46 @@
 import "dotenv/config"
-// import { PrismaClient } from "@prisma/client";
-// import { PrismaPg } from "@prisma/adapter-pg";
+import { PrismaClient } from "@/generated/client"
+import { PrismaClient } from "@prisma/client";
+import { PrismaPg } from "@prisma/adapter-pg";
 
 
-// const connectionString = `${process.env.DATABASE_URL}`;
+const connectionString = `${process.env.DATABASE_URL}`;
 
 
 
-// const globalForPrisma = global as unknown as {
-//     prisma: PrismaClient | undefined;
-// };
+const globalForPrisma = global as unknown as {
+    prisma: PrismaClient | undefined;
+};
 
-// const adapter = new PrismaPg({connectionString})
+const adapter = new PrismaPg({connectionString})
 
-// export const prisma =
-//   globalForPrisma.prisma || new PrismaClient({adapter});
+export const prisma =
+  globalForPrisma.prisma || new PrismaClient({adapter});
 
-// if (process.env.NODE_ENV !== "production") {
-//   globalForPrisma.prisma = prisma;
-// }
+if (process.env.NODE_ENV !== "production") {
+  globalForPrisma.prisma = prisma;
+}
 
 
 
 // import { PrismaClient } from '@prisma/client'
 
-import { PrismaClient } from "@/generated/client"
-import { PrismaPg } from '@prisma/adapter-pg'
-import { Pool } from 'pg'
+// import { PrismaPg } from '@prisma/adapter-pg'
+// import { Pool } from 'pg'
 
-const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: true
-})
+// const pool = new Pool({
+//   connectionString: process.env.DATABASE_URL,
+//   ssl: true
+// })
 
-const adapter = new PrismaPg(pool)
+// const adapter = new PrismaPg(pool)
 
-export const prisma =
-  globalThis.prisma ??
-  new PrismaClient({
-    adapter
-  })
+// export const prisma =
+//   globalThis.prisma ??
+//   new PrismaClient({
+//     adapter
+//   })
 
-if (process.env.NODE_ENV !== 'production') {
-  globalThis.prisma = prisma
-}
+// if (process.env.NODE_ENV !== 'production') {
+//   globalThis.prisma = prisma
+// }
