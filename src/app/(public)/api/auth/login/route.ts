a75cb63d383b514ba0d7ffbf9f2d15f1bca3f7a3
TejasJuniwal13 <tejasjuniwal13@gmail.com>
2026-03-08 20:44:25 +0530

    added

--- src/app/(public)/api/auth/login/route.ts
+++ src/app/(public)/api/auth/login/route.ts
@@ -5,25 +5,28 @@ import { NextResponse } from "next/server";
 
 
 export async function POST(req: Request) {
-  const { email, password } = await req.json();
+  try {
 
-  // find user by email
-  const user = await prisma.user.findUnique({
-    where : {email},
-  })
-
-  if (!user) {
+    const { email, password } = await req.json();
+    
+    
+    // find user by email
+    const user = await prisma.user.findUnique({
+      where : {email},
+    })
+    
+    if (!user) {
     return Response.json({ error: "Invalid credentials" }, { status: 401 });
   }
-
+  
   // plain text password comparison
   if (user.password !== password) {
     return Response.json({ error: "Invalid credentials" }, { status: 401 });
   }
-
+  
   const accessToken = await generateAccessToken(user.id , user.role , user.email);
   const refreshToken = await generateRefreshToken(user.id);
-
+  
   const response  = NextResponse.json(
     {
       accessToken:accessToken,
@@ -32,34 +35,39 @@ export async function POST(req: Request) {
     },
     {status:200}
   );
-
+  
   // save role in cookie
   response.cookies.set("role", user.role, {
     httpOnly: true,
     sameSite: "strict",
     path: "/",
   });
-
-   response.cookies.set("accessToken", accessToken, {
-            httpOnly: true,
-            secure: true,
-            sameSite: "strict",
-            path: "/",
-            maxAge: 15 * 60, // 15 minutes
-        });
-
-
-        // set refresh token in HTTP-only cookies 
-        response.cookies.set("refreshToken", refreshToken, {
-            httpOnly: true,
-            secure: true,
-            sameSite: "strict",
-            path: "/",
-            maxAge: 7 * 24 * 60 * 60 // 7 days,
-        });
-
-
-
+  
+  response.cookies.set("accessToken", accessToken, {
+    httpOnly: true,
+    secure: true,
+    sameSite: "strict",
+    path: "/",
+    maxAge: 15 * 60, // 15 minutes
+  });
+  
+  
+  // set refresh token in HTTP-only cookies 
+  response.cookies.set("refreshToken", refreshToken, {
+    httpOnly: true,
+    secure: true,
+    sameSite: "strict",
+    path: "/",
+    maxAge: 7 * 24 * 60 * 60 // 7 days,
+  });
+  
+  
+  
   // success
   return response;
+
+} catch (error) {
+  console.log(error);
+}
+  
 }
